3a4e82257e71e082d5f455d9771fad342b9f2b2b
huey6402 <41720159+huey6402@users.noreply.github.com>
2018-07-31 14:14:27 +0800

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("    *    ");
+               Console.WriteLine("   ***   ");
+               Console.WriteLine("  *****  ");
+               Console.WriteLine(" ******* ");
+               Console.WriteLine("*********"); */
+            int i ;
+            string x ;
+            i = 9 ;
+            x = "*" ;
+
+            while (i <= 9) 
+            {
+                if (i = 9)
+                {
+
+                }
+                Console.WriteLine("");
+                i = i - 2 ; 
+            }
+            Console.WriteLine("END");
+        }
+    }
+    
+}
+        
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("    *    ");
+               Console.WriteLine("   ***   ");
+               Console.WriteLine("  *****  ");
+               Console.WriteLine(" ******* ");
+               Console.WriteLine("*********"); */
+            int a , b ;
+            string x , z ;
+            a = 1 ;
+            x = "*" ;
+            z = " " ;
+            while (a <= 5) 
+            {
+                {
+                    if (a < 5) { Console.Write(z); }
+                    if (a < 4) { Console.Write(z); }
+                    if (a < 3) { Console.Write(z); }
+                    if (a < 2) { Console.Write(z); }
+                    Console.Write(x);
+                    if (a > 1) { Console.Write(x+x); }
+                    if (a > 2) { Console.Write(x+x); }
+                    if (a > 3) { Console.Write(x+x); }
+                    if (a > 4) { Console.Write(x+x); }
+                    /*if (a < 2) { Console.Write(z); }
+                    if (a < 3) { Console.Write(z); }
+                    if (a < 4) { Console.Write(z); }
+                    if (a < 5) { Console.Write(z); }*/
+                }
+                Console.WriteLine("");
+                a = a + 1 ; 
+            }
+            //Console.WriteLine("END");
+        }
+    }
+    
+}
+        
